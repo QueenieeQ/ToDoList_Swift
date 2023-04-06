04aacffa10735699bbedcd854fba5dbc7bdcd1ca
QueenieeQ <xuanquyhnvn@gmail.com>
2023-04-06 20:21:27 +0700

    add listview to navigationView

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,13 +5,26 @@
 //  Created by Quý Ninh on 06/04/2023.
 //
 
+/*
+ 
+ some brief about MVVC architecture
+ 
+ Model - Data point
+ View - UI
+ ViewMode- view model can manage data in model for view
+ 
+ */
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
+            NavigationView(){
+                ListView()
+                //            entire app is always in a navigation view
+            }
         }
     }
 }
